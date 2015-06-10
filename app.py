192e54824089046d7261fce9e0b2c02bb96a9dbb
Anthony Carminati <anthony@carminati.io>
2015-06-10 00:10:24 -0500

    testing copy - carriage return issue

--- app.py
+++ app.py
@@ -89,24 +89,25 @@ for file in ftp_files:
 for file in os.listdir(g['DATA_DROP_PATH']):
     # DAILY REPORTS
     if '_Daily' in file:
-        # pass
         # CALCULATE NUMBER OF ROWS
         # num_rows = sum(1 for row in csv.reader(open('{0}{1}'.format(g['DATA_DROP_PATH'], file), mode='r')))
         # TAKE ALL RECORDS EXCEPT FOR LAST ONE AND WRITE TO DATA_CONVERTED_PATH FOLDER
         # REMOVE OLD FILE FROM DATA_DROP_PATH
         try:
-            with open('{0}{1}'.format(g['DATA_DROP_PATH'], file), 'rb') as file_obj:
-                logger.info('Cleaning {0}.'.format(file))
-                file_obj.seek(0, os.SEEK_END)
-                pos = file_obj.tell() - 1
-                while pos > 0 and file_obj.read(1) != "\n":
-                    pos -= 1
-                    file_obj.seek(pos, os.SEEK_SET)
-                if pos > 0:
-                    file_obj.seek(pos, os.SEEK_SET)
-                    file_obj.truncate()
+            # with open('{0}{1}'.format(g['DATA_DROP_PATH'], file), 'rb+') as file_obj:
+            #     logger.info('Cleaning {0}.'.format(file))
+            #     file_obj.seek(0, os.SEEK_END)
+            #     pos = file_obj.tell() - 1
+            #     while pos > 0 and file_obj.read(1) != "\n":
+            #         pos -= 1
+            #         file_obj.seek(pos, os.SEEK_SET)
+            #     if pos > 0:
+            #         file_obj.seek(pos, os.SEEK_SET)
+            #         file_obj.truncate()
 
-                shutil.move('{0}{1}'.format(g['DATA_DROP_PATH'], file), '{0}{1}'.format(g['DATA_CONVERTED_PATH'], file))
+            subprocess.call(['wc', '-l', '{0}'.format('{0}{1}'.format(g['DATA_DROP_PATH'], file))])
+
+            # shutil.move('{0}{1}'.format(g['DATA_DROP_PATH'], file), '{0}{1}'.format(g['DATA_CONVERTED_PATH'], file))
 
         except Exception, e:
             logger.error('{0}. Could not clean {1}'.format(e, file))
@@ -118,7 +119,6 @@ for file_str in os.listdir(g['DATA_CONVERTED_PATH']):
         try:
             sql_cmd = u'\"\\COPY stg_daily_trades(trader,sequence_no,account,side,symbol,quantity,price,destination,contra,trade_datetime,bo_account,cusip,liq,order_id,exec_broker,ecn_fee,order_datetime,specialist,commission,bb_trade,sec_fee,batch_id,client_order_id,prime,cover_quantity,userr,settle_date,principal,net_amount,allocation_id,allocation_role,is_clearable,nscc_fee,nasdaq_fee,clearing_fee,nyse_etf_fee,amex_etf_fee,listing_exchange,native_liq,order_received_id,bo_group_id) FROM \'{q_file_path}\' WITH DELIMITER \',\' CSV HEADER ESCAPE AS \'\\\' \"'.format(q_file_path=g['DATA_CONVERTED_PATH']+file_str)
             pgsql_cmd = u'sudo psql {pg_user} -h {pg_host} -d {pg_db} -p 5432 -c {sql_cmd}'.format(pg_user=g['POSTGRES_USER'], pg_host=g['POSTGRES_HOST'], pg_db=g['POSTGRES_DB'], sql_cmd=sql_cmd)
-            print pgsql_cmd
             pgsql_status = subprocess.call(pgsql_cmd, shell=True)
         except Exception, e:
             logger.error('{0}. {1} could not be pushed to database'.format(e, file_str))
