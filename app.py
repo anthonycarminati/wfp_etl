84af9c3d54ddf4e81ae9a7744c3dea2a9ae470bf
Anthony Carminati <anthony@carminati.io>
2015-06-10 21:01:31 -0500

    copy working. testing auditing

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ for file in ftp_files:
 # CLEAN UP FILE FROM DROP FOLDER AND PLACE IN CONVERTED FOLDER FOR UPLOAD
 for file in os.listdir(g['DATA_DROP_PATH']):
     # DAILY REPORTS
-    if '_Daily' in file and file not in exclude_file:
+    if '_Daily' in file:
         try:
             # READ, CLEAN, AND WRITE CONVERTED DATA
             data_in = pd.read_csv('{0}{1}'.format(g['DATA_DROP_PATH'], file))
@@ -105,7 +105,7 @@ for file in os.listdir(g['DATA_DROP_PATH']):
 # PUSH ALL DOWNLOADS TO DATABASE
 for file in os.listdir(g['DATA_FINAL_PATH']):
     # P&L REPORTS
-    if '_Daily' in file and file not in exclude_file:
+    if '_Daily' in file:
         try:
             sql_cmd = u'\"\\COPY stg_daily_trades(trader,sequence_no,account,side,symbol,quantity,price,destination,contra,trade_datetime,bo_account,cusip,liq,order_id,exec_broker,ecn_fee,order_datetime,specialist,commission,bb_trade,sec_fee,batch_id,client_order_id,prime,cover_quantity,userr,settle_date,principal,net_amount,allocation_id,allocation_role,is_clearable,nscc_fee,nasdaq_fee,clearing_fee,nyse_etf_fee,amex_etf_fee,listing_exchange,native_liq,order_received_id,bo_group_id) FROM \'{q_file_path}\' WITH CSV HEADER DELIMITER \',\' \"'.format(q_file_path='{0}{1}'.format(g['DATA_FINAL_PATH'], file))
             pgsql_cmd = u'sudo psql {pg_user} -h {pg_host} -d {pg_db} -p 5432 -c {sql_cmd}'.format(pg_user=g['POSTGRES_USER'], pg_host=g['POSTGRES_HOST'], pg_db=g['POSTGRES_DB'], sql_cmd=sql_cmd)
