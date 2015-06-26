1950e93d7dafb7dbb50ad5db1a759351dcdb0c19
Anthony Carminati <anthony@carminati.io>
2015-06-25 20:29:41 -0400

    testing psycopg2 implementation

--- app.py
+++ app.py
@@ -108,7 +108,7 @@ for file in os.listdir(g['DATA_FINAL_PATH']):
             with open('{0}{1}'.format(g['DATA_FINAL_PATH'], file), 'rb') as copy_file:
                 sql_cmd = """COPY stg_daily_trades(trader,sequence_no,account,side,symbol,quantity,price,destination,contra,trade_datetime,bo_account,cusip,liq,order_id,exec_broker,ecn_fee,order_datetime,specialist,commission,bb_trade,sec_fee,batch_id,client_order_id,prime,cover_quantity,userr,settle_date,principal,net_amount,allocation_id,allocation_role,is_clearable,nscc_fee,nasdaq_fee,clearing_fee,nyse_etf_fee,amex_etf_fee,listing_exchange,native_liq,order_received_id,bo_group_id) FROM STDIN WITH CSV HEADER DELIMITER AS ','"""
                 cur.copy_expert(sql_cmd, copy_file)
-                cur.commit()
+                conn.commit()
 
             # # LOGGING
             logger.info('Successfully pushed {0} to database.'.format(file))
