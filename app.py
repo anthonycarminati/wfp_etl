716aacd0f0d6ae39c344a03e6d2ccd9ea83a6efa
Anthony Carminati <anthony@carminati.io>
2015-07-09 11:58:38 -0400

    postgres push working. still in dev mode so uncommenting os.remove statements is still required.

--- app.py
+++ app.py
@@ -192,8 +192,15 @@ for file in os.listdir(g['DATA_FINAL_PATH']):
             num_rows = sum(1 for line in open('{0}{1}'.format(g['DATA_FINAL_PATH'], file))) - 1
 
             # COMPOSE AND EXECUTE AUDITING RECORD
+            conn = ps.connect(host=g['POSTGRES_HOST'],
+                  port='5432',
+                  user=g['POSTGRES_USER'],
+                  password=g['POSTGRES_PWD'],
+                  database=g['POSTGRES_DB'])
+                cur = conn.cursor()
             sql_cmd = """INSERT INTO etl_daily_trades(file_name, file_size, num_rows) VALUES(%(file_name)s, %(file_size)s, %(num_rows)s);"""
             cur.execute(sql_cmd, {'file_name': file, 'file_size': file_size, 'num_rows': num_rows})
+            conn.commit()
 
             # LOGGING
             logger.info('Successfully wrote auditing record for {0}'.format(file))
